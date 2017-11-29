77acea091725e329d8c53e4f8bf82193a92d52d6
wyzheng <879335251@qq.com>
2017-11-28 22:29:53 -0800

    download lab2

--- kern/kdebug.c
+++ kern/kdebug.c
@@ -47,6 +47,8 @@ extern const char __STABSTR_END__[];		// End of string table
 //		stab_binsearch(stabs, &left, &right, N_SO, 0xf0100184);
 //	will exit setting left = 118, right = 554.
 //
+
+
 static void
 stab_binsearch(const struct Stab *stabs, int *region_left, int *region_right,
 	       int type, uintptr_t addr)
@@ -179,7 +181,7 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 	//	Look at the STABS documentation and <inc/stab.h> to find
 	//	which one.
 	// Your code here.
-
+        
         stab_binsearch(stabs, &lline, &rline, N_SLINE, addr);
         if (lline > rline)
              return -1;
--- lib/printfmt.c
+++ lib/printfmt.c
@@ -206,7 +206,10 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 		// (unsigned) octal
 		case 'o':
 			// Replace this with your code.
-			num = getuint(&ap, lflag);
+			//putch('0', putdat);
+			//putch('X', putdat);
+			//putch('X', putdat);
+                        num = getuint(&ap, lflag);
 			base = 8;
 			goto number;
 			//break;
